acf589bfc15efedb498b7e418906b9eef7d43cf2
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-06 09:36:13 +0200

    update slide effect to match players data. hid the aside element on player click

--- frontend/fanatii-football-x/src/Components/ClubsPages/ClubsPage.jsx
+++ frontend/fanatii-football-x/src/Components/ClubsPages/ClubsPage.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import './ClubsPage.css'
+import './ClubsPage.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import Navbar from '../Global_Navbar/Navbar';
+import { useRef } from 'react';
 
 //this component will render the specific clubs data
 //and make a request via the clubs name, which is passed by state
 function ClubsPage({ clubName }) {
-  const [playerBackendData, setplayerBackendData] = useState( );
+  const [playerBackendData, setplayerBackendData] = useState();
   const [playerStats, setPlayerStats] = useState();
-  const [playerSection, setPlayerSection] = useState('Players-club-pages-section-full')
-  const [asideWidth, setAside]  = useState('player-stats-none');
-  
-
+  const [playerSection, setPlayerSection] = useState(
+    'Players-club-pages-section-full'
+  );
+  const [asideWidth, setAside] = useState('player-stats-none');
+  const [refKey, setRefKey] = useState();
+  const prevRefKey = useRef('');
   //fetch club data after component has loaded
   useEffect(() => {
     fetch(`/GlobalSearch/ClubSearch/${clubName}`)
@@ -26,21 +29,27 @@ function ClubsPage({ clubName }) {
       });
   }, [clubName]);
 
-
   //onclick passes the players 'key', so it can be used as reference to the players data, when we want to reveal the stats of the player
-  const revealStats = (e, key) =>{
+  const revealStats = (e, key) => {
     e.preventDefault();
-    console.log(key)
-    if(playerSection === 'Players-club-pages-section-full'){
-      setAside('player-stats-half')
-      setPlayerSection('Players-club-pages-section-half')
+    setRefKey(key);
+    prevRefKey.current = refKey;
+
+    if (prevRefKey.current === key) {
+      if (asideWidth === 'player-stats-none') {
+        setAside('player-stats-half')
+        setPlayerSection('Players-club-pages-section-half')
+      } else {
+        setAside('player-stats-none')
+        setPlayerSection('Players-club-pages-section-full')
+      }
+    } else {
+      setAside('player-stats-half');
+      setPlayerSection('Players-club-pages-section-half');
     }
-    else{
-      setAside('player-stats-none')
-      setPlayerSection('Players-club-pages-section-full')
-    }
-    setPlayerStats(key)
-  }
+
+    setPlayerStats(key);
+  };
 
   return (
     <>
@@ -51,67 +60,97 @@ function ClubsPage({ clubName }) {
           <Navbar idNav='nav-search' />
 
           <div id='Players-Club-Pages-Container'>
-
-           <div id="Player-stats-flex-wrapper">
-           <section id={playerSection}>
+            <div id='Player-stats-flex-wrapper'>
+              <section id={playerSection}>
                 {playerBackendData.map((player, key) => {
                   return (
                     <div className='player-main-container'>
-                      <div className='player-wrapper' key={key} onClick={(event)=> revealStats(event, key)}>
-                      <div className='club-player-img-container'>
-                        <img src={player.player_image} alt="player_image" className="club-player"/>
-                      </div>
+                      <div
+                        className='player-wrapper'
+                        key={key}
+                        onClick={(event) => revealStats(event, key)}
+                      >
+                        <div className='club-player-img-container'>
+                          <img
+                            src={player.player_image}
+                            alt='player_image'
+                            className='club-player'
+                          />
+                        </div>
 
-                      <div className='club-player-description'>
-                        <p>Name:<span className='player-info'>{player.name}</span></p>
-                        <p>DoB:<span className='player-info'> {player.dateOfBirth}</span></p>
-                        <p>Nationality:<span className='player-info'>{player.nationality}</span></p>
-                        <p>Position:<span className='player-info'>{player.position}</span></p>
+                        <div className='club-player-description'>
+                          <p>
+                            Name:
+                            <span className='player-info'>{player.name}</span>
+                          </p>
+                          <p>
+                            DoB:
+                            <span className='player-info'>
+                              {' '}
+                              {player.dateOfBirth}
+                            </span>
+                          </p>
+                          <p>
+                            Nationality:
+                            <span className='player-info'>
+                              {player.nationality}
+                            </span>
+                          </p>
+                          <p>
+                            Position:
+                            <span className='player-info'>
+                              {player.position}
+                            </span>
+                          </p>
+                        </div>
                       </div>
-                    </div>
                     </div>
                   );
                 })}
-            </section>
+              </section>
 
-            
+              {typeof playerStats === 'undefined' ? (
+                <></>
+              ) : (
+                <aside id={asideWidth}>
+                  <div id='aside-image-wrapper'>
+                    <button id='view-more'>+</button>
+                    <img
+                      src={playerBackendData[playerStats].player_image}
+                      alt='player-aside'
+                      className='aside-image'
+                    />
+                  </div>
 
-           {
-            (typeof playerStats === 'undefined') ? (
-              <></>
-            ) : (
-              <aside id={asideWidth}>
-              <div id="aside-image-wrapper">
-              <button id="view-more">+</button>
-                <img src={playerBackendData[playerStats].player_image} alt="player-aside" className="aside-image" />
-              </div>
-              
-              <div className="aside-stats-wrapper">
-                <h4 className='aside-player-info'>Name: {playerBackendData[playerStats].name}</h4>
-                <h4 className='aside-player-info'>Position: {playerBackendData[playerStats].position}</h4>               
-                <h4 className='aside-player-info'>Nationality: {playerBackendData[playerStats].nationality}</h4> 
-                <h4 className='aside-player-info'>DoB: {playerBackendData[playerStats].dateOfBirth}</h4>
+                  <div className='aside-stats-wrapper'>
+                    <h4 className='aside-player-info'>
+                      Name: {playerBackendData[playerStats].name}
+                    </h4>
+                    <h4 className='aside-player-info'>
+                      Position: {playerBackendData[playerStats].position}
+                    </h4>
+                    <h4 className='aside-player-info'>
+                      Nationality: {playerBackendData[playerStats].nationality}
+                    </h4>
+                    <h4 className='aside-player-info'>
+                      DoB: {playerBackendData[playerStats].dateOfBirth}
+                    </h4>
 
-                <h4 id="Stats-heading">Summary: </h4>
-                {
-                  playerBackendData[playerStats].stats.map((stats, key)=>{
-                    return(
-                      <div className='Stats' key={key}>                              
-                      <p className='stat'>{stats.split(' ')[0]}</p>
-                      <div className="bar">
-                        <div className={`meter-${key}`}></div>
-                      </div>
-                      </div>
-                    )
-                  })
-                }
-
-              </div>
-            </aside>
-            )
-           }
-           </div>
-
+                    <h4 id='Stats-heading'>Summary: </h4>
+                    {playerBackendData[playerStats].stats.map((stats, key) => {
+                      return (
+                        <div className='Stats' key={key}>
+                          <p className='stat'>{stats.split(' ')[0]}</p>
+                          <div className='bar'>
+                            <div className={`meter-${key}`}></div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </aside>
+              )}
+            </div>
           </div>
         </>
       )}
