1c9697ead00b99e93bc9363687caaa63f6e0769a
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-08 16:04:10 +0200

    removed unnecessary code from the backend to improve code readability

--- backend/controller/FindPlayers.js
+++ backend/controller/FindPlayers.js
@@ -1,12 +1,10 @@
 const PlayersModel  = require('../models/Players');
 
 exports.findPlayer = async function(req, res){
-    console.log(req.body.playerName)
     await PlayersModel.find({
         name: req.body.playerName
     })
     .then((data)=>{
-        console.log(data[0])
         if(typeof data[0] === 'undefined'){
             res.status(500).send('player not found')
         }
