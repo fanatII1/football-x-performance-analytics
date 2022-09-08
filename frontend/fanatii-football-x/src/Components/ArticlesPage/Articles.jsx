56d9b3bbef7f09b97b54af49a1c1eabbde308dfc
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-08 10:49:31 +0200

    redesigned articles page

--- frontend/fanatii-football-x/src/Components/ArticlesPage/Articles.jsx
+++ frontend/fanatii-football-x/src/Components/ArticlesPage/Articles.jsx
@@ -4,21 +4,27 @@ import Navbar from '../Global_Navbar/Navbar'
 import PassNet from './ArticlesImages/pass-net.jpg'
 import Shot from './ArticlesImages/shot.png'
 import XA from './ArticlesImages/assists(3).png'
-import ballProgression from './ArticlesImages/ball-progression.jpg'
+import ballProgression from './ArticlesImages/ball-progression.png'
 import scoutTalent from './ArticlesImages/talent-scout.jpg'
 import PodCastOne from './ArticlesImages/podcast(1).jpg'
 import PodCastTwo from './ArticlesImages/podcast(2).jpg'
 import PodCastThree from './ArticlesImages/podcast(3).png'
 import PodCastFour from './ArticlesImages/podcast(4).png'
+import Stats from './ArticlesImages/stats.png'
+import Player2 from './ArticlesImages/player2.jpg'
+import Corner from './ArticlesImages/corner_data.png'
 import Footer from '../Global_Footer/Footer'
 import GlobalNavBottom from '../GlobalNavBottom/GlobalNavBottom'
-import ArticleBackgroundImage from './ArticlesImages/article-background.jpg'
 
 const articlesData = [
     {image: PassNet, heading: "Analysing the passing networks of each Dstv Premiership team", journalist: "Reece Moore"},
     {image: scoutTalent, heading: "Talent Scout: Cape Town City midfielder, Taahir Goedeman(analysis)?", journalist: "Jason McAllister"},
     {image: XA, heading: "The truth behind expected assists(xA)", journalist: "Njabulo Mokoena"},
-    {image: Shot, heading: "Should clubs focus on shot quality using expected goals?", journalist: "Carol Hallie"}
+    {image: Shot, heading: "Should clubs focus on shot quality using expected goals?", journalist: "Carol Hallie"},
+    {image: ballProgression, heading: "Unpacking the secrets behind ball progression", journalist: "Sbusiso Biyela"},
+    {image: Stats, heading: "Player Analysis: The rise of Peter Shalulile", journalist: "Julia Cox"},
+    {image: Player2, heading: "Thembinkosi Lorch: The African Raumdeuter", journalist: "George Fanatii"},
+    {image: Corner, heading: "DYK: 30% of goals are scored after set pieces", journalist: "George Fanatii"}
 ];
 
 const podcastData = [
@@ -41,13 +47,13 @@ function Articles() {
                 {
                     articlesData.map((article, key)=>{
                         return(
-                            <div className="article">
+                            <div className={`article a-${key}`}>
                                 <div className="article-image-wrapper">
                                     <img src={article.image} alt="" className="article-image" />
                                 </div>
 
                                 <div className="article-short-summary">
-                                    <h4 className="journalist">Writer: {article.journalist}</h4>
+                                    <h4 className="journalist">Writer : {article.journalist} </h4>
                                     <h4 className="article-heading">{article.heading}</h4>
                                     <p className="article-intro-paragraph">
                                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat itaque id dolor? Minima consequatur ipsa amet inventore, perspiciatis itaque eaque aspernatur?
@@ -68,14 +74,18 @@ function Articles() {
                 {
                     podcastData.map((article, key)=>{
                         return(
-                        <div className='podcast-sub-' key={key}> 
+                        <div className='podcast-sub' key={key}> 
                         <div className="sub-main-podcast-image-wrapper">
                             <img src={article.image} alt="sub-article" className="article-sub-sec-image" />
                         </div>
     
                         <div className="sub-main-podcast-text-wrapper">
-                            <h5 className="sub-podcast-article-heading">{article.heading} <i className="fa-solid fa-podcast"></i></h5>
-                            <p className='journalist'>
+                            <h5 className="sub-podcast-article-heading">
+                                <a href="https://podcasts.apple.com/us/podcast/the-back-three/id1582621804">
+                                {article.heading} <i className="fa-solid fa-podcast"></i>
+                                </a>
+                            </h5>
+                            <p className='host'>
                                 Hosts: 
                                 <br />
                                 {article.host}
