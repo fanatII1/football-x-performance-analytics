ea96af8ee9315a848c506e4a7225d4cd8fe4998f
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-27 15:33:53 +0200

    added bars width values based on player stats values

--- frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
+++ frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './ModalClubPages.css';
-import statsImage from './analytics4.png';
 
 //modal Component(child) for the ClubPages Component
 function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats }) {
@@ -8,7 +7,30 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
   //key will be used to reference players data from backend data
   let playerImage = (typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].player_image);
   let playerAllStats = playerBackendData[playerStats];
-  console.log(playerAllStats)
+
+  //we want the bar to escalate its value width based on the actual stat value
+  //we need a function that will check the stat value, based on it, we'll adjust width of bar
+  function barWidth(actualStat){
+    let actualStat_num = parseFloat(actualStat);
+    console.log(actualStat)
+    if(actualStat < 0.3){
+      console.log('below 0.3')
+      return {width: '25%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    }
+    else if(actualStat_num >= 0.3 && actualStat_num <= 0.5){
+      console.log('between 0.3 and 0.5')
+      return {width: '50%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    }
+    else if(actualStat_num >= 0.6 && actualStat_num <= 0.8){
+      console.log('between 0.6 and 0.8')
+      return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    }
+    else{
+      console.log('above 0.8')
+      return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    }
+  }
+
   
   return (
     <div id={modalState}>
@@ -20,11 +42,14 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
         </div>
 
         <div id='detailed-stats-container'>
+          <h2 id="DetailedStatsHeading">Statistics:</h2>
           {(typeof playerAllStats === 'undefined') ? (<></>) :
            (
             playerAllStats.stats.map((stat)=>{
               let statName = stat.split(':')[0];
               let actualStat = stat.split(':')[1];
+              
+              let bar_widthStyles = barWidth(actualStat);
 
               return(
               <div className="stat-view">
@@ -34,7 +59,7 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
                   <h5 className="actual-stat">{actualStat}</h5>
                 </div>
                 
-                <div className="stat-bar">...</div>
+                <div className="stat-bar" style={bar_widthStyles}> </div>
               </div>
               )
             })
