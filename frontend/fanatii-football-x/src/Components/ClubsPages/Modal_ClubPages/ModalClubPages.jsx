d6c4ff19a27363836ed46d1432a6730a5099967d
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-27 15:40:07 +0200

    added the players name on the modal and their position

--- frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
+++ frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
@@ -3,30 +3,27 @@ import './ModalClubPages.css';
 
 //modal Component(child) for the ClubPages Component
 function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats }) {
-  //when click specific player, we wanto modal to view that players data, thus from theplayer backend, we pass the players key
+  //when click specific player, we  want a view that players data
+  //thus we pass the players key(integer)
   //key will be used to reference players data from backend data
   let playerImage = (typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].player_image);
   let playerAllStats = playerBackendData[playerStats];
 
-  //we want the bar to escalate its value width based on the actual stat value
-  //we need a function that will check the stat value, based on it, we'll adjust width of bar
+  //function that will set stats bar width based on stats of player
+  //returns an object that has properties that define the styles that will be applied to the bar
   function barWidth(actualStat){
     let actualStat_num = parseFloat(actualStat);
-    console.log(actualStat)
+
     if(actualStat < 0.3){
-      console.log('below 0.3')
       return {width: '25%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
     }
     else if(actualStat_num >= 0.3 && actualStat_num <= 0.5){
-      console.log('between 0.3 and 0.5')
       return {width: '50%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
     }
     else if(actualStat_num >= 0.6 && actualStat_num <= 0.8){
-      console.log('between 0.6 and 0.8')
       return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
     }
     else{
-      console.log('above 0.8')
       return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
     }
   }
@@ -43,6 +40,8 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
 
         <div id='detailed-stats-container'>
           <h2 id="DetailedStatsHeading">Statistics:</h2>
+          <h3 id="player-name">Name: {playerAllStats.name}</h3>
+          <h4 id="player-position">Position: {playerAllStats.position}</h4>
           {(typeof playerAllStats === 'undefined') ? (<></>) :
            (
             playerAllStats.stats.map((stat)=>{
