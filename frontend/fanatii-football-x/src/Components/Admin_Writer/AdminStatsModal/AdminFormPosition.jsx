0c3c188ad6d95694f5d72187947173e2fd265c41
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-27 08:53:30 +0200

    removed auto complete and added padding to enlarge inner of of forms

--- frontend/fanatii-football-x/src/Components/Admin_Writer/AdminStatsModal/AdminFormPosition.jsx
+++ frontend/fanatii-football-x/src/Components/Admin_Writer/AdminStatsModal/AdminFormPosition.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function FormPosition({position}){
     const [userData, setUserData] = useState({});
@@ -27,26 +27,26 @@ function FormPosition({position}){
 
     if(position === 'GK'){
         return(
-            <form id='GKForm' onSubmit={submitStats}>
-                <h2 id="position-heading">{position} :</h2>
-                <div id="form-main-content">
-                <div className="form1">
-                <label htmlFor="name">Name:</label>
+            <form id='GKForm' onSubmit={submitStats} autoComplete='off'>
+                <h2 id='position-heading'>{position} :</h2>
+                <div id='form-main-content'>
+                <div className='form1'>
+                <label htmlFor='name'>Name:</label>
                 <input type='text' name='name' id='name' placeholder='Name' onChange={handleChange}/>
-                <label htmlFor="position">Position:</label>
+                <label htmlFor='position'>Position:</label>
                 <input type='text' name='position'id='position' placeholder='Position' onChange={handleChange}/>
-                <label htmlFor="club">Club:</label>
+                <label htmlFor='club'>Club:</label>
                 <input type='text' name='club' id='club'  placeholder='Club' onChange={handleChange}/>
-                <label htmlFor="DefActionsOutsidePen">Defensive Actions Outside Pen. :</label>
+                <label htmlFor='DefActionsOutsidePen'>Defensive Actions Outside Pen. :</label>
                 <input type='text' name='DefActionsOutsidePen' id='DefActionsOutsidePen' placeholder='DefActionsOutsidePen' onChange={handleChange}/>
                 </div>
 
-                <div className="form2">
-                <label htmlFor="GoalAgainst">Goals Against:</label>
+                <div className='form2'>
+                <label htmlFor='GoalAgainst'>Goals Against:</label>
                 <input type='text' name='GoalAgainst' id='GoalAgainst' placeholder='GoalAgainst' onChange={handleChange}/>
-                <label htmlFor="PSxGGA">PSxGGA:</label>
+                <label htmlFor='PSxGGA'>PSxGGA:</label>
                 <input type='text' name='PSxGGA' id='PSxGGA' placeholder='PSxGGA' onChange={handleChange}/>
-                <label htmlFor="PSxGSoT">PSxGSoT</label>
+                <label htmlFor='PSxGSoT'>PSxGSoT</label>
                 <input type='text' name='PSxGSoT' id='PSxGSoT' placeholder='PSxGSoT' onChange={handleChange}/>
                 </div>
                 </div>
@@ -56,28 +56,28 @@ function FormPosition({position}){
     }
     else if(position === 'DEF'){
         return(
-            <form id='DEFForm' onSubmit={submitStats}>
-                <h2 id="position-heading">{position} :</h2>
-                <div id="form-main-content">
-                <div className="form1">
-                <label htmlFor="name">Name: </label>
+            <form id='DEFForm' onSubmit={submitStats} autoComplete='off'>
+                <h2 id='position-heading'>{position} :</h2>
+                <div id='form-main-content'>
+                <div className='form1'>
+                <label htmlFor='name'>Name: </label>
                 <input type='text' id='name' name='name' placeholder='Name' onChange={handleChange}/>
-                <label htmlFor="position">Position:</label>
+                <label htmlFor='position'>Position:</label>
                 <input type='text' id='position' name='position' placeholder='Position' onChange={handleChange}/>
-                <label htmlFor="club">Club:</label>
+                <label htmlFor='club'>Club:</label>
                 <input type='text' id='club' name='club' placeholder='Club' onChange={handleChange}/>
-                <label htmlFor="TacklesPer90">Tackels Per 90:</label>
+                <label htmlFor='TacklesPer90'>Tackels Per 90:</label>
                 <input type='text' name='TacklesActionsPer90' id='TacklesPer90' placeholder='TacklesPer90' onChange={handleChange}/>
                 </div>
 
-                <div className="form2">
-                <label htmlFor="DEfensiveActionsPer90">Defensive Actions Per 90:</label>
+                <div className='form2'>
+                <label htmlFor='DEfensiveActionsPer90'>Defensive Actions Per 90:</label>
                 <input type='text' name='DefensiveActionsPer90' id='DefensiveActionsPer90' placeholder='DefActionsPer90' onChange={handleChange}/>
-                <label htmlFor="AerialDeadBalls">Aerial Deadballs won: </label>
+                <label htmlFor='AerialDeadBalls'>Aerial Deadballs won: </label>
                 <input type='text' name='AerialDeadBalls' id='AerialDeadBalls' placeholder='AerialDeadBalls' onChange={handleChange}/>
-                <label htmlFor="AttackingOutput">Attacking Output:</label>
+                <label htmlFor='AttackingOutput'>Attacking Output:</label>
                 <input type='text' name='AttackingOutput' id='AttackingOutput' placeholder='AttackingOutput' onChange={handleChange}/>
-                <label htmlFor="BallRetention">Ball Retention:</label>
+                <label htmlFor='BallRetention'>Ball Retention:</label>
                 <input type='text' name='BallRetention' id='BallRetention' placeholder='BallRetention' onChange={handleChange}/>
                 </div>
                 </div>
@@ -87,42 +87,42 @@ function FormPosition({position}){
     }
     else if(position === 'MID'){
         return(
-            <form id='MIDForm' onSubmit={submitStats}>
-                <h2 id="position-heading">{position} :</h2>
-                <div id="form-main-content">                
-                <div className="form1">
-                <label htmlFor="name">Name:</label>
+            <form id='MIDForm' onSubmit={submitStats} autoComplete='off'>
+                <h2 id='position-heading'>{position} :</h2>
+                <div id='form-main-content'>                
+                <div className='form1'>
+                <label htmlFor='name'>Name:</label>
                 <input type='text' id='name' name='name' placeholder='Name' onChange={handleChange}/>
-                <label htmlFor="position">Position</label>
+                <label htmlFor='position'>Position</label>
                 <input type='text' id='position' name='position' placeholder='Position' onChange={handleChange}/>
-                <label htmlFor="club">Club:</label>
+                <label htmlFor='club'>Club:</label>
                 <input type='text' id='club' name='club'placeholder='Club' onChange={handleChange}/>
-                <label htmlFor="xG">xG(Expected Goals):</label>
+                <label htmlFor='xG'>xG(Expected Goals):</label>
                 <input type='text' name='xG' id='xG' placeholder='xG' onChange={handleChange}/>
-                <label htmlFor="xA">xA(Expected Assists):</label>
+                <label htmlFor='xA'>xA(Expected Assists):</label>
                 <input type='text' name='xA' id='xA' placeholder='xA' onChange={handleChange}/>
-                <label htmlFor="shoot">Shooting: </label>
+                <label htmlFor='shoot'>Shooting: </label>
                 <input type='text' name='shoot' id='shoot' placeholder='Shoot' onChange={handleChange}/>
-                <label htmlFor="Defensive Actions Per 90">Defensive Actions Per 90:</label>
+                <label htmlFor='Defensive Actions Per 90'>Defensive Actions Per 90:</label>
                 <input type='text' name='DefensiveActionsPer90' id='DefensiveActionsPer90' placeholder='DefActionsPer90' onChange={handleChange}/>
-                <label htmlFor="Linkup">Linkup:</label>
+                <label htmlFor='Linkup'>Linkup:</label>
                 <input type='text' name='Linkup' id='Linkup' placeholder='Linkup' onChange={handleChange}/>
                 </div>
 
-                <div className="form2"> 
-                <label htmlFor="AttackingOutput">Attacking Output:</label>
+                <div className='form2'> 
+                <label htmlFor='AttackingOutput'>Attacking Output:</label>
                 <input type='text' name='AttackingOutput' id='AttackingOutput' placeholder='AttackingOutput' onChange={handleChange}/>
-                <label htmlFor="BallRetention">Ball Retention:</label>
+                <label htmlFor='BallRetention'>Ball Retention:</label>
                 <input type='text' name='BallRetention' id='BallRetention' placeholder='BallRetention' onChange={handleChange}/>
-                <label htmlFor="Dribble">Dribbling:</label>
+                <label htmlFor='Dribble'>Dribbling:</label>
                 <input type='text' name='Dribble' id='Dribble' placeholder='Dribble' onChange={handleChange}/>
-                <label htmlFor="Aerial">Aerial:</label>
+                <label htmlFor='Aerial'>Aerial:</label>
                 <input type='text' name='Aerial' id='Aerial' placeholder='Aerial' onChange={handleChange}/>
-                <label htmlFor="Recover">Recover:</label>
+                <label htmlFor='Recover'>Recover:</label>
                 <input type='text' name='Recover' id='Recover' placeholder='Recover' onChange={handleChange}/>
-                <label htmlFor="PassTowardsGoal">Pass Towards Goal:</label>
+                <label htmlFor='PassTowardsGoal'>Pass Towards Goal:</label>
                 <input type='text' name='PassTowardsGoal' id='PassTowardsGoal' placeholder='PassTowardsGoal' onChange={handleChange}/>
-                <label htmlFor="ReceiveInBox">Recieve In Box:</label>
+                <label htmlFor='ReceiveInBox'>Recieve In Box:</label>
                 <input type='text' name='RecieveInBox' id='RecieveInBox' placeholder='RecieveInBox' onChange={handleChange}/>
                 </div>
                 </div>
@@ -132,42 +132,42 @@ function FormPosition({position}){
     }
     else if(position === 'WING'){
         return(
-            <form id='WINGForm' onSubmit={submitStats}>
-                <h2 id="position-heading">{position} :</h2>
-                <div id="form-main-content">                
-                <div className="form1">
-                <label htmlFor="name">Name:</label>
+            <form id='WINGForm' onSubmit={submitStats} autoComplete='off'>
+                <h2 id='position-heading'>{position} :</h2>
+                <div id='form-main-content'>                
+                <div className='form1'>
+                <label htmlFor='name'>Name:</label>
                 <input type='text' id='name' name='name' placeholder='Name' onChange={handleChange}/>
-                <label htmlFor="position">Position</label>
+                <label htmlFor='position'>Position</label>
                 <input type='text' id='position' name='position' placeholder='Position' onChange={handleChange}/>
-                <label htmlFor="club">Club:</label>
+                <label htmlFor='club'>Club:</label>
                 <input type='text' id='club' name='club'placeholder='Club' onChange={handleChange}/>
-                <label htmlFor="xG">xG(Expected Goals):</label>
+                <label htmlFor='xG'>xG(Expected Goals):</label>
                 <input type='text' name='xG' id='xG' placeholder='xG' onChange={handleChange}/>
-                <label htmlFor="xA">xA(Expected Assists):</label>
+                <label htmlFor='xA'>xA(Expected Assists):</label>
                 <input type='text' name='xA' id='xA' placeholder='xA' onChange={handleChange}/>
-                <label htmlFor="shoot">Shooting: </label>
+                <label htmlFor='shoot'>Shooting: </label>
                 <input type='text' name='shoot' id='shoot' placeholder='Shoot' onChange={handleChange}/>
-                <label htmlFor="Defensive Actions Per 90">Defensive Actions Per 90:</label>
+                <label htmlFor='Defensive Actions Per 90'>Defensive Actions Per 90:</label>
                 <input type='text' name='DefensiveActionsPer90' id='DefensiveActionsPer90' placeholder='DefActionsPer90' onChange={handleChange}/>
-                <label htmlFor="Linkup">Linkup:</label>
+                <label htmlFor='Linkup'>Linkup:</label>
                 <input type='text' name='Linkup' id='Linkup' placeholder='Linkup' onChange={handleChange}/>
                 </div>
 
-                <div className="form2"> 
-                <label htmlFor="AttackingOutput">Attacking Output:</label>
+                <div className='form2'> 
+                <label htmlFor='AttackingOutput'>Attacking Output:</label>
                 <input type='text' name='AttackingOutput' id='AttackingOutput' placeholder='AttackingOutput' onChange={handleChange}/>
-                <label htmlFor="BallRetention">Ball Retention:</label>
+                <label htmlFor='BallRetention'>Ball Retention:</label>
                 <input type='text' name='BallRetention' id='BallRetention' placeholder='BallRetention' onChange={handleChange}/>
-                <label htmlFor="Dribble">Dribbling:</label>
+                <label htmlFor='Dribble'>Dribbling:</label>
                 <input type='text' name='Dribble' id='Dribble' placeholder='Dribble' onChange={handleChange}/>
-                <label htmlFor="Aerial">Aerial:</label>
+                <label htmlFor='Aerial'>Aerial:</label>
                 <input type='text' name='Aerial' id='Aerial' placeholder='Aerial' onChange={handleChange}/>
-                <label htmlFor="Recover">Recover:</label>
+                <label htmlFor='Recover'>Recover:</label>
                 <input type='text' name='Recover' id='Recover' placeholder='Recover' onChange={handleChange}/>
-                <label htmlFor="PassTowardsGoal">Pass Towards Goal:</label>
+                <label htmlFor='PassTowardsGoal'>Pass Towards Goal:</label>
                 <input type='text' name='PassTowardsGoal' id='PassTowardsGoal' placeholder='PassTowardsGoal' onChange={handleChange}/>
-                <label htmlFor="ReceiveInBox">Recieve In Box:</label>
+                <label htmlFor='ReceiveInBox'>Recieve In Box:</label>
                 <input type='text' name='RecieveInBox' id='RecieveInBox' placeholder='RecieveInBox' onChange={handleChange}/>
                 </div>
                 </div>
@@ -177,42 +177,42 @@ function FormPosition({position}){
     }
     else{
         return(
-            <form id='STForm' onSubmit={submitStats}>
-                <h2 id="position-heading">{position} :</h2>
-                <div id="form-main-content">                
-                <div className="form1">
-                <label htmlFor="name">Name:</label>
+            <form id='STForm' onSubmit={submitStats} autoComplete='off'>
+                <h2 id='position-heading'>{position} :</h2>
+                <div id='form-main-content'>                
+                <div className='form1'>
+                <label htmlFor='name'>Name:</label>
                 <input type='text' id='name' name='name' placeholder='Name' onChange={handleChange}/>
-                <label htmlFor="position">Position</label>
+                <label htmlFor='position'>Position</label>
                 <input type='text' id='position' name='position' placeholder='Position' onChange={handleChange}/>
-                <label htmlFor="club">Club:</label>
+                <label htmlFor='club'>Club:</label>
                 <input type='text' id='club' name='club'placeholder='Club' onChange={handleChange}/>
-                <label htmlFor="xG">xG(Expected Goals):</label>
+                <label htmlFor='xG'>xG(Expected Goals):</label>
                 <input type='text' name='xG' id='xG' placeholder='xG' onChange={handleChange}/>
-                <label htmlFor="xA">xA(Expected Assists):</label>
+                <label htmlFor='xA'>xA(Expected Assists):</label>
                 <input type='text' name='xA' id='xA' placeholder='xA' onChange={handleChange}/>
-                <label htmlFor="shoot">Shooting: </label>
+                <label htmlFor='shoot'>Shooting: </label>
                 <input type='text' name='shoot' id='shoot' placeholder='Shoot' onChange={handleChange}/>
-                <label htmlFor="Defensive Actions Per 90">Defensive Actions Per 90:</label>
+                <label htmlFor='Defensive Actions Per 90'>Defensive Actions Per 90:</label>
                 <input type='text' name='DefensiveActionsPer90' id='DefensiveActionsPer90' placeholder='DefActionsPer90' onChange={handleChange}/>
-                <label htmlFor="Linkup">Linkup:</label>
+                <label htmlFor='Linkup'>Linkup:</label>
                 <input type='text' name='Linkup' id='Linkup' placeholder='Linkup' onChange={handleChange}/>
                 </div>
 
-                <div className="form2"> 
-                <label htmlFor="AttackingOutput">Attacking Output:</label>
+                <div className='form2'> 
+                <label htmlFor='AttackingOutput'>Attacking Output:</label>
                 <input type='text' name='AttackingOutput' id='AttackingOutput' placeholder='AttackingOutput' onChange={handleChange}/>
-                <label htmlFor="BallRetention">Ball Retention:</label>
+                <label htmlFor='BallRetention'>Ball Retention:</label>
                 <input type='text' name='BallRetention' id='BallRetention' placeholder='BallRetention' onChange={handleChange}/>
-                <label htmlFor="Dribble">Dribbling:</label>
+                <label htmlFor='Dribble'>Dribbling:</label>
                 <input type='text' name='Dribble' id='Dribble' placeholder='Dribble' onChange={handleChange}/>
-                <label htmlFor="Aerial">Aerial:</label>
+                <label htmlFor='Aerial'>Aerial:</label>
                 <input type='text' name='Aerial' id='Aerial' placeholder='Aerial' onChange={handleChange}/>
-                <label htmlFor="Recover">Recover:</label>
+                <label htmlFor='Recover'>Recover:</label>
                 <input type='text' name='Recover' id='Recover' placeholder='Recover' onChange={handleChange}/>
-                <label htmlFor="PassTowardsGoal">Pass Towards Goal:</label>
+                <label htmlFor='PassTowardsGoal'>Pass Towards Goal:</label>
                 <input type='text' name='PassTowardsGoal' id='PassTowardsGoal' placeholder='PassTowardsGoal' onChange={handleChange}/>
-                <label htmlFor="ReceiveInBox">Recieve In Box:</label>
+                <label htmlFor='ReceiveInBox'>Recieve In Box:</label>
                 <input type='text' name='RecieveInBox' id='RecieveInBox' placeholder='RecieveInBox' onChange={handleChange}/>
                 </div>
                 </div>
