96ee91bb1f489b7db258590958c2a8257e35bc35
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-09-28 07:50:10 +0200

    styled the stat bar and added dynamic width of the stat bar based on user value

--- frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
+++ frontend/fanatii-football-x/src/Components/ClubsPages/Modal_ClubPages/ModalClubPages.jsx
@@ -6,25 +6,30 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
   //when click specific player, we  want a view that players data
   //thus we pass the players key(integer)
   //key will be used to reference players data from backend data
-  let playerImage = (typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].player_image);
   let playerAllStats = playerBackendData[playerStats];
+  let playerName =(typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].name);
+  let playerPosition = (typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].position);
+  let playerImage = (typeof playerStats === 'undefined' ? '' : playerBackendData[playerStats].player_image);
 
   //function that will set stats bar width based on stats of player
   //returns an object that has properties that define the styles that will be applied to the bar
   function barWidth(actualStat){
     let actualStat_num = parseFloat(actualStat);
 
-    if(actualStat < 0.3){
+    if(actualStat < 0.39){
       return {width: '25%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
     }
-    else if(actualStat_num >= 0.3 && actualStat_num <= 0.5){
-      return {width: '50%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    else if(actualStat_num > 0.39 && actualStat_num < 0.59){
+      return {width: '50%', background: 'linear-gradient(79deg, #474bff 0%, #bc48ff 100%)'}
     }
-    else if(actualStat_num >= 0.6 && actualStat_num <= 0.8){
-      return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+    else if(actualStat_num > 0.59 && actualStat_num < 0.79){
+      return {width: '75%', background: 'linear-gradient(79deg, #474bff 0%, #bc48ff 100%)'}
+    }
+    else if(actualStat_num > 0.59 && actualStat_num < 0.90){
+      return {width: '88%', background: 'linear-gradient(79deg, #474bff 0%, #bc48ff 100%)'}
     }
     else{
-      return {width: '75%', background: 'linear-gradient(90deg, #ff00e2 0%, #ff008e 100%)'}
+      return {width: '95%', background: 'linear-gradient(79deg, #474bff 0%, #bc48ff 100%)'}
     }
   }
 
@@ -40,18 +45,18 @@ function ModalClubPages({ modalState, closeModal, playerBackendData, playerStats
 
         <div id='detailed-stats-container'>
           <h2 id="DetailedStatsHeading">Statistics:</h2>
-          <h3 id="player-name">Name: {playerAllStats.name}</h3>
-          <h4 id="player-position">Position: {playerAllStats.position}</h4>
+          <h3 id="player-name">Name: {playerName}</h3>
+          <h4 id="player-position">Position: {playerPosition}</h4>
           {(typeof playerAllStats === 'undefined') ? (<></>) :
            (
-            playerAllStats.stats.map((stat)=>{
+            playerAllStats.stats.map((stat, key)=>{
               let statName = stat.split(':')[0];
               let actualStat = stat.split(':')[1];
               
               let bar_widthStyles = barWidth(actualStat);
 
               return(
-              <div className="stat-view">
+              <div className="stat-view" key={key}>
     
                 <div className="stat-description">
                   <h5 className="stat-name">{statName}</h5>
