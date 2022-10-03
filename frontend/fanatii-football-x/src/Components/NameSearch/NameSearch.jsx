bc93f8edcad2987fad12ced8606c95c00d45c920
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-10-03 09:51:48 +0200

    added blockage to preven user from searching for players if the have not signed in

--- frontend/fanatii-football-x/src/Components/NameSearch/NameSearch.jsx
+++ frontend/fanatii-football-x/src/Components/NameSearch/NameSearch.jsx
@@ -5,6 +5,7 @@ import Avatar from './NameSearchImages/avatar.webp'
 import Footer from '../Global_Footer/Footer'
 import GlobalNavBottom from '../GlobalNavBottom/GlobalNavBottom'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function NameSearch() {
     const [playerName, setPlayerName] = useState('');
@@ -36,37 +37,45 @@ function NameSearch() {
         })
     }
 
-  return (
-    <>
-        <Navbar idNav='nav-search' />
-        <div id='Main-Search-player-container'>
 
-            <div id='search-container'>
-                <div id='search-input-wrapper'>
-                    <form id='search-form' method='GET' action='/GlobalSearch/NameSearch'>
-                    <input type='text' className='search-field' onChange={handleChange} value={'' || playerName} placeholder='Search Player...'/>
-                    <input type='button' value='Search' className='submit-search' onClick={SearchPlayerEnter}/>
-                    </form>
-                </div>
-
-                <div id='searched-player'>
-                    <div className='searched-player-image-wrapper'>
-                        <img src={typeof playerBackendData === 'undefined' ? Avatar : playerBackendData[0].player_image } alt='player' className='searched-image' />
+    if(localStorage.getItem('adminToken') || localStorage.getItem('userToken')){
+        if(typeof playerBackendData !== 'undefined'){
+            return (
+                <>
+                    <Navbar idNav='nav-search' />
+                    <div id='Main-Search-player-container'>
+            
+                        <div id='search-container'>
+                            <div id='search-input-wrapper'>
+                                <form id='search-form' method='GET' action='/GlobalSearch/NameSearch'>
+                                <input type='text' className='search-field' onChange={handleChange} value={'' || playerName} placeholder='Search Player...'/>
+                                <input type='button' value='Search' className='submit-search' onClick={SearchPlayerEnter}/>
+                                </form>
+                            </div>
+            
+                            <div id='searched-player'>
+                                <div className='searched-player-image-wrapper'>
+                                    <img src={typeof playerBackendData === 'undefined' ? Avatar : playerBackendData[0].player_image } alt='player' className='searched-image' />
+                                </div>
+                                <div className='searched-player-summary'>
+                                    <p className="player-name">Name: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].name}</span></p>
+                                    <p className="player-dOb">Date of Birth: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].dateOfBirth}</span></p>
+                                    <p className="player-nationality">Nationality: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].nationality}</span></p>
+                                    <p className="player-position">Position: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].position}</span></p>
+                                </div>
+                            </div>
+                        </div>
+            
                     </div>
-                    <div className='searched-player-summary'>
-                        <p className="player-name">Name: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].name}</span></p>
-                        <p className="player-dOb">Date of Birth: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].dateOfBirth}</span></p>
-                        <p className="player-nationality">Nationality: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].nationality}</span></p>
-                        <p className="player-position">Position: <span className='player-searched-info'>{typeof playerBackendData === 'undefined' ? ' ' : playerBackendData[0].position}</span></p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-        <Footer idFooter='home-footer'/>
-        <GlobalNavBottom navBottom='articles-nav-bottom'/>
-    </>
-  )
+                    <Footer idFooter='home-footer'/>
+                    <GlobalNavBottom navBottom='articles-nav-bottom'/>
+                </>
+              )
+        }
+    }
+    else{
+      return <p style={{color: 'white'}}><Link to='/SignUp'>Please Sign Up</Link></p>
+    }
 }
 
 export default NameSearch
