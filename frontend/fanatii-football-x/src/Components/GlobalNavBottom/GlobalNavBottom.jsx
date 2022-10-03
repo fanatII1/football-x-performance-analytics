845c2f993bed515f3744c730fd9a3bcc07517b2c
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2022-10-03 14:35:17 +0200

    restyled the navbar bottom bg color

--- frontend/fanatii-football-x/src/Components/GlobalNavBottom/GlobalNavBottom.jsx
+++ frontend/fanatii-football-x/src/Components/GlobalNavBottom/GlobalNavBottom.jsx
@@ -1,40 +1,64 @@
-import React from 'react'
-import './GlobalNavBottom.css'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import './GlobalNavBottom.css';
+import { Link } from 'react-router-dom';
 
-function GlobalNavBottom({navBottom}) {
+function GlobalNavBottom({ navBottom }) {
   return (
     <div id={navBottom}>
-        <ul id='bottom-nav-list'>
+      <ul id='bottom-nav-list'>
+        <li className='list'>
+          <Link to='/'>
+            <span className='icon'>
+              <i className='fa-solid fa-house'></i>
+            </span>
+            <span className='text'>Home</span>
+          </Link>
+        </li>
+        <li className='list'>
+          <Link to='/GlobalSearch'>
+            <span className='icon'>
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </span>
+            <span className='text'>Search</span>
+          </Link>
+        </li>
+        <li className='list'>
+          <Link to='/Articles'>
+            <span className='icon'>
+              <i className='fa-solid fa-newspaper'></i>
+            </span>
+            <span className='text'>Articles</span>
+          </Link>
+        </li>
+        {localStorage.getItem('adminToken') ? (
+          <li className='list'>
+            <Link to='/Admin'>
+              <span className='icon'>
+                <i className="fa-solid fa-user-plus"></i>
+              </span>
+              <span className='text'>Admin</span>
+            </Link>
+          </li>
+        ) : (
+          <></>
+        )}
+        {localStorage.getItem('adminToken') || localStorage.getItem('userToken') ? (
+          <>
             <li className='list'>
-                <Link to="/">
-                    <span className="icon">
-                    <i className='fa-solid fa-house'></i>
-                    </span>
-                    <span className="text">
-                        Home
-                    </span>
-                </Link>
+              <Link to='/Login'>
+                <span className='icon'>
+                    <i class="fa-solid fa-right-to-bracket"></i>
+                </span>
+                <span className='text'>Articles</span>
+              </Link>
             </li>
-            <li className='list'>
-                <Link to="/GlobalSearch">
-                    <span className="icon">
-                        <i className='fa-solid fa-magnifying-glass'></i>
-                    </span>
-                    <span className="text">Search</span>
-                </Link>
-            </li>
-            <li className='list'>
-                <Link to="/Articles">
-                    <span className="icon">
-                        <i className='fa-solid fa-newspaper'></i>
-                    </span>
-                    <span className="text">Articles</span>
-                </Link>
-            </li>
-        </ul>
+          </>
+        ):(
+            <></>
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default GlobalNavBottom
+export default GlobalNavBottom;
